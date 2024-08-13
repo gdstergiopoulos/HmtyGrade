54e7013e783e78a02faeb0a049f9c0db84b7617a
gdstergiopoulos <gdstergiopoulos@gmail.com>
2024-08-13 13:38:59 +0300

    conversion to node with hbs

---
+++ app.mjs
@@ -0,0 +1,44 @@
+// Express.js
+import express from 'express'
+// Handlebars (https://www.npmjs.com/package/express-handlebars)
+import { engine } from 'express-handlebars'
+import session from 'express-session';
+
+
+
+
+const app = express()
+const router = express.Router();
+const port = process.env.PORT || '3000';
+
+app.use(express.static('public'));
+app.engine('hbs', engine({ extname: 'hbs' }));
+app.set('view engine', 'hbs');
+
+
+app.use(router);
+router.use(express.urlencoded({ extended: true }));
+
+router.route('/').get((req, res) => {
+    res.render('main');
+});
+
+router.route('/login').get((req, res) => {
+    res.render('login');
+});
+
+router.route('/register').get((req, res) => {
+    res.render('register');
+});
+
+router.route('/home').get((req, res) => {
+    res.redirect('/');
+});
+
+router.use((req, res) => {
+    res.render('catcherror');
+});
+
+const PORT=process.env.PORT || 3000;
+const server = app.listen(PORT, () => { console.log(`http://127.0.0.1:${PORT}`) });
+
